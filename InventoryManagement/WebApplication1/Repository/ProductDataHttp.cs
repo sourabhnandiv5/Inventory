8cce60d1b323b6c4f7895a9d8affd75386ef7520
Moumita <dasmou08@Gmail.com>
2022-03-20 20:59:10 +0000

    add

---
+++ InventoryManagement/WebApplication1/Repository/ProductDataHttp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Repository
+{
+    public class ProductDataHttp
+    {
+        string url = "https://localhost:44300/api/product";
+
+        public async void getproduct()
+        {
+            using (var httpclient = new HttpClient())
+            {
+             var data =  await  httpclient.GetStringAsync(url);
+            }
+        }
+    }
+}
